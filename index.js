ac7602d2732a9d2794c9d7778570d6befe99e05d
Abel S Mathew <abelsmathew228@gmail.com>
2025-03-06 23:45:54 +0530

    Implemented basic Express server with multiple routes

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-console.log('Hello from the first backend code!');
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.get('/twitter', (req, res) => {
+    res.send('Hello Twitter!')
+})
+
+app.get('/login', (req, res) => {
+    res.send('<h1>Login</h1>')
+})
+
+app.get('/youtube', (req, res) => {
+    res.send('<h2>Hello Youtube!</h2>')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
